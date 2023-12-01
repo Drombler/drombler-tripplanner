40a81a2d69f658964b01a8b710f31457ba64c177
Florian Brunner <fbrunnerreg@gmx.ch>
2023-12-01 22:54:39 +0100

    inital Vaadin application

---
+++ src/main/java/org/drombler/tripplanner/DromblerTripplannerApplication.java
@@ -0,0 +1,13 @@
+package org.drombler.tripplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DromblerTripplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DromblerTripplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/drombler/tripplanner/DromblerTripplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.drombler.tripplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DromblerTripplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
